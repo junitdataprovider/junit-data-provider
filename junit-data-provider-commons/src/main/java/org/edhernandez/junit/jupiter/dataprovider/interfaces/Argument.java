4261b430aa5af089299db3313b33b6908ee5d524
Eduardo Hernandez <eduardo.hernandez@almundo.com>
2019-05-12 01:25:44 -0300

    create json module

--- junit-data-provider-commons/src/main/java/org/edhernandez/junit/jupiter/dataprovider/interfaces/Argument.java
+++ junit-data-provider-commons/src/main/java/org/edhernandez/junit/jupiter/dataprovider/interfaces/Argument.java
@@ -1,4 +1,17 @@
 package org.edhernandez.junit.jupiter.dataprovider.interfaces;
 
-public interface Argument {
+import java.util.List;
+
+public abstract class Argument {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Argument setName(String name) {
+        this.name = name;
+        return this;
+    }
 }
--- junit-data-provider-jackson/src/main/java/org/edhernandez/junit/jupiter/dataprovider/jackson/JsonArgument.java
+++ junit-data-provider-jackson/src/main/java/org/edhernandez/junit/jupiter/dataprovider/jackson/JsonArgument.java
@@ -2,5 +2,5 @@ package org.edhernandez.junit.jupiter.dataprovider.jackson;
 
 import org.edhernandez.junit.jupiter.dataprovider.interfaces.Argument;
 
-public class JsonArgument implements Argument {
+public class JsonArgument extends Argument {
 }
